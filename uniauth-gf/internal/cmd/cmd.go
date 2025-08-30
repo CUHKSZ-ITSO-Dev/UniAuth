5579c1bc96372afbd19e5c84929330f2b643c7a6
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-08-30 23:26:12 +0800

    换用新版swagger UI

--- uniauth-gf/internal/cmd/cmd.go
+++ uniauth-gf/internal/cmd/cmd.go
@@ -15,25 +15,31 @@ const (
 	SwaggerUITemplate = `
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="SwaggerUI"/>
+    <meta name="description" content="SwaggerUI" />
     <title>SwaggerUI</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.10.5/swagger-ui.min.css" />
-</head>
-<body>
-<div id="swagger-ui"></div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.10.5/swagger-ui-bundle.js" crossorigin></script>
-<script>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
+  </head>
+  <body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-standalone-preset.js" crossorigin></script>
+  <script>
     window.onload = () => {
-        window.ui = SwaggerUIBundle({
-            url:    '{SwaggerUIDocUrl}',
-            dom_id: '#swagger-ui',
-        });
+      window.ui = SwaggerUIBundle({
+        url: '/api.json',
+        dom_id: '#swagger-ui',
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout",
+      });
     };
-</script>
-</body>
+  </script>
+  </body>
 </html>
 `
 )
