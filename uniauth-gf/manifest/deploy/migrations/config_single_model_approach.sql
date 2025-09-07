2872b0c9359671f2888bc4933f846b033f2bdfdc
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-09-07 18:21:16 +0800

    SQL表建立

---
+++ uniauth-gf/manifest/deploy/migrations/config_single_model_approach.sql
@@ -0,0 +1,11 @@
+CREATE TABLE config_single_model_approach (
+    model_name VARCHAR(255) PRIMARY KEY,
+    price JSONB,
+    discount DECIMAL(15, 10),
+    client_type VARCHAR(255),
+    client_args JSONB,
+    request_args JSONB,
+    servicewares VARCHAR(255)[]
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
---
+++ uniauth-gf/manifest/deploy/migrations/quotapool_quota_pool.sql
@@ -0,0 +1,16 @@
+CREATE TABLE quotapool_quota_pool (
+    id BIGSERIAL PRIMARY KEY,
+
+    quota_pool_name VARCHAR(255) NOT NULL UNIQUE, -- 配额池名称
+    cron_cycle VARCHAR(255) NOT NULL, -- 刷新周期
+    regular_quota DECIMAL(20, 6) NOT NULL, -- 定期配额
+    remaining_quota DECIMAL(20, 6) NOT NULL, -- 剩余配额
+    last_reset_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 上次刷新时间
+    extra_quota DECIMAL(20, 6) NOT NULL DEFAULT 0, -- 加油包
+    personal BOOLEAN NOT NULL, -- 是否个人配额池
+    disabled BOOLEAN NOT NULL, -- 是否禁用
+
+    userinfos_rules JSONB, -- 用户信息规则
+
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
