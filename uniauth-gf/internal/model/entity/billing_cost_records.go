c97682ae60e45f2a9dc8b2c6a14935542204554d
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-09-08 14:07:27 +0800

    新建cost_records表，统一使用NUMERIC(25,10)

--- uniauth-gf/internal/model/entity/billing_cost_records.go
+++ uniauth-gf/internal/model/entity/billing_cost_records.go
@@ -6,17 +6,18 @@ package entity
 
 import (
 	"github.com/gogf/gf/v2/os/gtime"
+	"github.com/shopspring/decimal"
 )
 
 // BillingCostRecords is the golang structure for table billing_cost_records.
 type BillingCostRecords struct {
-	Id        int64       `json:"id"        orm:"id"         description:""` //
-	Upn       string      `json:"upn"       orm:"upn"        description:""` //
-	Svc       string      `json:"svc"       orm:"svc"        description:""` //
-	Product   string      `json:"product"   orm:"product"    description:""` //
-	Cost      float64     `json:"cost"      orm:"cost"       description:""` //
-	Plan      string      `json:"plan"      orm:"plan"       description:""` //
-	Source    string      `json:"source"    orm:"source"     description:""` //
-	Remark    string      `json:"remark"    orm:"remark"     description:""` //
-	CreatedAt *gtime.Time `json:"createdAt" orm:"created_at" description:""` //
+	Id        int64           `json:"id"        orm:"id"         description:""` //
+	Upn       string          `json:"upn"       orm:"upn"        description:""` //
+	Svc       string          `json:"svc"       orm:"svc"        description:""` //
+	Product   string          `json:"product"   orm:"product"    description:""` //
+	Cost      decimal.Decimal `json:"cost"      orm:"cost"       description:""` //
+	Plan      string          `json:"plan"      orm:"plan"       description:""` //
+	Source    string          `json:"source"    orm:"source"     description:""` //
+	Remark    string          `json:"remark"    orm:"remark"     description:""` //
+	CreatedAt *gtime.Time     `json:"createdAt" orm:"created_at" description:""` //
 }
