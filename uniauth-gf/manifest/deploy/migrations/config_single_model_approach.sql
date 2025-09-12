e08cc39c160a35767c79f7c7f5454d0574123f90
BetterAndBetterII <1176445047@qq.com>
2025-09-12 19:31:33 +0800

    修复SQL语法错误，将servicewares字段的定义从VARCHAR(255)[]更正为VARCHAR(255)[]

--- uniauth-gf/manifest/deploy/migrations/config_single_model_approach.sql
+++ uniauth-gf/manifest/deploy/migrations/config_single_model_approach.sql
@@ -5,7 +5,7 @@ CREATE TABLE config_single_model_approach (
     client_type VARCHAR(255),
     client_args JSONB,
     request_args JSONB,
-    servicewares VARCHAR(255)[]
+    servicewares VARCHAR(255)[],
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
