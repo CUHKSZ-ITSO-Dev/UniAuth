0e3f2738c8d6f9208f761a157e140730529e90e3
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-09-12 11:09:52 +0800

    尝试支持多用户、多配额池导出

--- uniauth-gf/api/billing/v1/admin.go
+++ uniauth-gf/api/billing/v1/admin.go
@@ -8,8 +8,9 @@ import (
 // admin：主要是处理账单导出、账单查询等功能
 
 type ExportBillRecordReq struct {
-	g.Meta     `path:"/admin/export" tags:"Billing/Admin" method:"post" summary:"导出账单" dc:"导出账单，根据一定的条件。"`
-	QuotaPool  string   `json:"quotaPool" v:"required" dc:"配额池" example:"student_pool"`
+	g.Meta     `path:"/admin/export" tags:"Billing/Admin" method:"post" summary:"导出账单" dc:"导出账单，根据一定的条件。有两个类型的账单：<br>1. 配额池出账（可以传Array），每个配额池的账单会在不同的工作表里面；<br>2. 个人出账（可以传Array），每个人的消费记录会在不同的工作表里面。<br>QuotaPool 数组和 UPN 数组只能同时传一个。"`
+	Upns       []string `json:"upns" dc:"UPN列表" example:"['122020255@link.cuhk.edu.cn']"`
+	QuotaPools []string `json:"quotaPools" dc:"配额池" example:"['student_pool']"`
 	Svc        []string `json:"svc" dc:"服务" example:"['chat', 'voice']"`
 	Product    []string `json:"product" dc:"产品" example:"['chat', 'voice']"`
 	StartTime  string   `json:"startTime" v:"required" dc:"开始时间" example:"2024-01-01"`
@@ -19,13 +20,14 @@ type ExportBillRecordRes struct {
 }
 
 type GetBillRecordReq struct {
-	g.Meta    `path:"/admin/get" tags:"Billing/Admin" method:"post" summary:"查询账单" dc:"查询账单，根据一定的条件。"`
-	QuotaPool string   `json:"quotaPool" v:"required" dc:"配额池" example:"student_pool"`
-	Svc       []string `json:"svc" dc:"服务" example:"['chat', 'voice']"`
-	Product   []string `json:"product" dc:"产品" example:"['chat', 'voice']"`
-	StartTime string   `json:"startTime" v:"required" dc:"开始时间" example:"2024-01-01"`
-	EndTime   string   `json:"endTime" v:"required" dc:"结束时间" example:"2024-01-01"`
+	g.Meta     `path:"/admin/get" tags:"Billing/Admin" method:"post" summary:"查询账单" dc:"查询账单，根据一定的条件。有两个类型的账单：<br>1. 配额池出账（可以传Array），每个配额池的账单会在不同的工作表里面；<br>2. 个人出账（可以传Array），每个人的消费记录会在不同的工作表里面。<br>QuotaPool 数组和 UPN 数组只能同时传一个。"`
+	Upns       []string `json:"upns" dc:"UPN列表" example:"['122020255@link.cuhk.edu.cn']"`
+	QuotaPools []string `json:"quotaPools" dc:"配额池" example:"['student_pool']"`
+	Svc        []string `json:"svc" dc:"服务" example:"['chat', 'voice']"`
+	Product    []string `json:"product" dc:"产品" example:"['chat', 'voice']"`
+	StartTime  string   `json:"startTime" v:"required" dc:"开始时间" example:"2024-01-01"`
+	EndTime    string   `json:"endTime" v:"required" dc:"结束时间" example:"2024-01-01"`
 }
 type GetBillRecordRes struct {
-	Records []*gjson.Json `json:"records"`
+	Records *gjson.Json `json:"records"`
 }
--- uniauth-gf/internal/controller/billing/billing_v1_get_bill_record.go
+++ uniauth-gf/internal/controller/billing/billing_v1_get_bill_record.go
@@ -6,27 +6,58 @@ import (
 	v1 "uniauth-gf/api/billing/v1"
 	"uniauth-gf/internal/dao"
 
+	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/gogf/gf/v2/errors/gerror"
 )
 
 func (c *ControllerV1) GetBillRecord(ctx context.Context, req *v1.GetBillRecordReq) (res *v1.GetBillRecordRes, err error) {
-	result, err := dao.BillingCostRecords.Ctx(ctx).
-		OmitEmpty().
-		Where("source", req.QuotaPool).
-		WhereIn("svc", req.Svc).
-		WhereIn("product", req.Product).
-		WhereGTE("created_at", req.StartTime).
-		WhereLTE("created_at", req.EndTime).
-		Order("created_at asc").
-		All()
-	if err != nil {
-		return nil, gerror.Wrap(err, "数据库查询账单记录时失败")
+	upnLen := len(req.Upns)
+	qpLen := len(req.QuotaPools)
+	if upnLen == 0 && qpLen == 0 {
+		return nil, gerror.New("UPNs 和 QuotaPools 必须传一个")
+	}
+	if !(upnLen > 0 && qpLen == 0 || upnLen == 0 && qpLen > 0) {
+		return nil, gerror.New("UPNs 和 QuotaPools 只能同时传一个")
+	}
+	resultMap := map[string]gdb.Result{}
+	if upnLen > 0 {
+		// upn 模式
+		for _, upn := range req.Upns {
+			result, err := dao.BillingCostRecords.Ctx(ctx).
+				OmitEmpty().
+				Where("upn", upn).
+				WhereIn("svc", req.Svc).
+				WhereIn("product", req.Product).
+				WhereGTE("created_at", req.StartTime).
+				WhereLTE("created_at", req.EndTime).
+				Order("created_at asc").
+				All()
+			if err != nil {
+				return nil, gerror.Wrapf(err, "[UPN 模式] 获取 UPN = %s 账单信息失败", upn)
+			}
+			resultMap[upn] = result
+		}
+	} else {
+		// Quota Pool 模式
+		for _, quotaPool := range req.QuotaPools {
+			result, err := dao.BillingCostRecords.Ctx(ctx).
+				OmitEmpty().
+				Where("source", quotaPool).
+				WhereIn("svc", req.Svc).
+				WhereIn("product", req.Product).
+				WhereGTE("created_at", req.StartTime).
+				WhereLTE("created_at", req.EndTime).
+				Order("created_at asc").
+				All()
+			if err != nil {
+				return nil, gerror.Wrapf(err, "[Quota Pool 模式] 获取 Source = %s 账单信息失败", quotaPool)
+			}
+			resultMap[quotaPool] = result
+		}
 	}
 
-	res = &v1.GetBillRecordRes{}
-	for _, record := range result {
-		res.Records = append(res.Records, gjson.New(record))
-	}
-	return
+	return &v1.GetBillRecordRes{
+		Records: gjson.New(resultMap),
+	}, nil
 }
