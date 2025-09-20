e5e69a2bb8742b7d14dfe6ed30a0a48f2021323e
Zitong Wu <mr.tom_123@outlook.com>
2025-09-20 14:00:10 -0700

    问题修复

--- uniauth-gf/internal/controller/config/config_v1_delete_model_config.go
+++ uniauth-gf/internal/controller/config/config_v1_delete_model_config.go
@@ -9,19 +9,22 @@ import (
 
 	v1 "uniauth-gf/api/config/v1"
 	"uniauth-gf/internal/dao"
+	"uniauth-gf/internal/model/entity"
 )
 
 func (c *ControllerV1) DeleteModelConfig(ctx context.Context, req *v1.DeleteModelConfigReq) (res *v1.DeleteModelConfigRes, err error) {
 	res = &v1.DeleteModelConfigRes{}
 
 	err = dao.ConfigSingleModelApproach.Transaction(ctx, func(ctx context.Context, tx gdb.TX) error {
-		_, err := dao.ConfigSingleModelApproach.Ctx(ctx).
-			Where("approach_name = ?", req.ApproachName).
-			LockUpdate().
-			Count()
+		var configSingleModelApproach *entity.ConfigSingleModelApproach
+		err = dao.ConfigSingleModelApproach.Ctx(ctx).Where("approach_name = ?", req.ApproachName).LockUpdate().Scan(&configSingleModelApproach)
 		if err != nil {
-			return gerror.WrapCode(gcode.CodeDbOperationError, err, "检查是否存在失败")
+			return gerror.Wrap(err, "查询模型配置信息失败")
 		}
+		if configSingleModelApproach == nil {
+			return gerror.Newf("该模型配置不存在，请重新检查：%v", req.ApproachName)
+		}
+
 		if _, err := dao.ConfigSingleModelApproach.Ctx(ctx).
 			Where("approach_name = ?", req.ApproachName).
 			Delete(); err != nil {
