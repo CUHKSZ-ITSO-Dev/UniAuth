3f634c37e60bbf73f7a40ea311ae356f1f819def
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-09-29 23:54:07 +0800

    增加定时任务每天在Casbin中更新配额池对应的用户

---
+++ uniauth-gf/internal/cronJobs/update_quotapools_users_in_casbin.go
@@ -0,0 +1,40 @@
+package cronjobs
+
+import (
+	"context"
+	"uniauth-gf/internal/dao"
+	"uniauth-gf/internal/model/entity"
+	"uniauth-gf/internal/service/quotaPool"
+
+	"github.com/gogf/gf/v2/database/gdb"
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
+)
+
+func UpdateQuotaPoolsUsersInCasbin(ctx context.Context, qpNameList *[]string) error {
+	if err := dao.QuotapoolQuotaPool.Transaction(ctx, func(ctx context.Context, tx gdb.TX) error {
+		var qps *[]entity.QuotapoolQuotaPool
+		if qpNameList == nil {
+			if err := dao.QuotapoolQuotaPool.Ctx(ctx).LockUpdate().Scan(&qps); err != nil {
+				return gerror.Wrap(err, "查询所有配额池失败")
+			} else if qps == nil {
+				return gerror.New("找不到要求的配额池")
+			}
+		} else {
+			if err := dao.QuotapoolQuotaPool.Ctx(ctx).LockUpdate().Where("quota_pool_name IN (?)", *qpNameList).Scan(&qps); err != nil {
+				return gerror.Wrap(err, "查询指定配额池失败")
+			} else if qps == nil {
+				return gerror.New("找不到要求的配额池")
+			}
+		}
+		for _, qp := range *qps {
+			if err := quotaPool.Edit(ctx, &qp); err != nil {
+				g.Log().Error(ctx, gerror.Wrapf(err, "更新配额池 %v 失败", qp.QuotaPoolName))
+			}
+		}
+		return nil
+	}); err != nil {
+		return gerror.Wrap(err, "定时任务：更新配额池用户组继承关系事务失败")
+	}
+	return nil
+}
