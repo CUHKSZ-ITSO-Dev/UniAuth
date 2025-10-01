48eb22b88b6f90cde09f8bbe7121700465f3fea2
NickWilde18 <95576172+NickWilde18@users.noreply.github.com>
2025-10-01 22:57:24 +0800

    Casbin PGSQL WatcherEx

--- uniauth-gf/internal/service/casbin/casbin.go
+++ uniauth-gf/internal/service/casbin/casbin.go
@@ -1,7 +1,10 @@
 package casbin
 
 import (
+	psqlwatcher "github.com/IguteChung/casbin-psql-watcher"
 	pgadapter "github.com/casbin/casbin-pg-adapter"
+
+	"context"
 
 	"github.com/casbin/casbin/v2"
 	"github.com/casbin/casbin/v2/model"
@@ -25,8 +28,14 @@ func init() {
 		panic("解析Casbin配置文件失败: " + err.Error())
 	}
 
-	// 数据库连接配置
 	ctx := gctx.New()
+
+	// Watcher 配置
+	conn := g.Cfg().MustGetWithEnv(ctx, "casbin.default.watcher").String()
+	w, _ := psqlwatcher.NewWatcherWithConnString(context.Background(), conn,
+		psqlwatcher.Option{Verbose: true})
+
+	// 数据库连接配置
 	dsn := g.Cfg().MustGetWithEnv(ctx, "casbin.default.link")
 	dbn := g.Cfg().MustGetWithEnv(ctx, "casbin.default.database")
 	a, err := pgadapter.NewAdapter(dsn.String(), dbn.String())
@@ -38,6 +47,16 @@ func init() {
 	e, err = casbin.NewEnforcer(m, a)
 	if err != nil {
 		panic("创建Casbin Enforcer失败: " + err.Error())
+	}
+
+	// Watcher
+	// 设置 Watcher
+	if err := e.SetWatcher(w); err != nil {
+		panic("设置Casbin Watcher失败: " + err.Error())
+	}
+	// Watcher 回调处理函数
+	if err := w.SetUpdateCallback(psqlwatcher.DefaultCallback(e)); err != nil {
+		panic("设置Casbin Watcher回调处理函数失败: " + err.Error())
 	}
 
 	// 加载策略
