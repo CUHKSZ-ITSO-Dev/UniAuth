b9307f4b029eb7c3a4144db66e800094841106e1
Zitong Wu <mr.tom_123@outlook.com>
2025-10-02 01:15:23 -0700

    完成删除quotapool接口服务化、casbin处理

--- uniauth-gf/internal/controller/quotaPool/quotaPool_v1_delete_quota_pool.go
+++ uniauth-gf/internal/controller/quotaPool/quotaPool_v1_delete_quota_pool.go
@@ -3,21 +3,16 @@ package quotaPool
 import (
 	"context"
 
-	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
 
 	v1 "uniauth-gf/api/quotaPool/v1"
-	"uniauth-gf/internal/dao"
+	"uniauth-gf/internal/service/quotaPool"
 )
 
 func (c *ControllerV1) DeleteQuotaPool(ctx context.Context, req *v1.DeleteQuotaPoolReq) (res *v1.DeleteQuotaPoolRes, err error) {
 	res = &v1.DeleteQuotaPoolRes{}
-	if sqlRes, delErr := dao.QuotapoolQuotaPool.Ctx(ctx).Where("quota_pool_name = ?", req.QuotaPoolName).Delete(); delErr != nil {
-		return nil, gerror.WrapCode(gcode.CodeDbOperationError, delErr, "删除配额池失败")
-	} else if eftRow, err := sqlRes.RowsAffected(); eftRow == 0 {
-		return nil, gerror.New("找不到配额池。数据库影响行数为0。")
-	} else if err != nil {
-		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, "检查配额池删除情况失败")
+	if err := quotaPool.Delete(ctx, req.QuotaPoolName); err != nil {
+		return nil, gerror.Wrap(err, "删除配额池失败")
 	}
 	res.OK = true
 	return
