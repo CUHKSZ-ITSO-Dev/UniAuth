9976c0b22c1a4d727661d9615e6b5402992f9a01
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-10-03 21:49:50 +0800

    grouping policies Filter

--- uniauth-gf/internal/controller/auth/auth_v1_filter_groupings.go
+++ uniauth-gf/internal/controller/auth/auth_v1_filter_groupings.go
@@ -2,30 +2,39 @@ package auth
 
 import (
 	"context"
+	"fmt"
+	"math"
+	"strings"
 
 	v1 "uniauth-gf/api/auth/v1"
-
-	"github.com/gogf/gf/v2/container/garray"
 )
 
 func (c *ControllerV1) FilterGroupings(ctx context.Context, req *v1.FilterGroupingsReq) (res *v1.FilterGroupingsRes, err error) {
-	groups, err := e.GetGroupingPolicy()
+	groupings, err := e.GetGroupingPolicy()
 	if err != nil {
 		return nil, err
 	}
 
-	upns := garray.NewStrArrayFrom(req.Upns)
-	roles := garray.NewStrArrayFrom(req.Roles)
-	res = &v1.FilterGroupingsRes{
-		Groupings: [][]string{},
+	var resGroupings = [][]string{}
+	for _, grouping := range groupings {
+		g1, g2 := grouping[0], grouping[1]
+		if strings.Contains(g1, req.G1) &&
+			strings.Contains(g2, req.G2) &&
+			strings.Contains(fmt.Sprintf("g, %v, %v", g1, g2), req.Rule) {
+			resGroupings = append(resGroupings, grouping)
+		}
 	}
 
-	for _, group := range groups {
-		user, role := group[0], group[1]
-		if (upns.Len() == 0 || upns.Contains(user)) &&
-			(roles.Len() == 0 || roles.Contains(role)) {
-			res.Groupings = append(res.Groupings, group)
-		}
+	res = &v1.FilterGroupingsRes{
+		Total:      len(resGroupings),
+		Page:       req.Page,
+		PageSize:   req.PageSize,
+		TotalPages: int(math.Ceil(float64(len(resGroupings)) / float64(req.PageSize))),
+	}
+	if req.Page*req.PageSize > len(resGroupings) {
+		res.Groupings = resGroupings[(req.Page-1)*req.PageSize:]
+	} else {
+		res.Groupings = resGroupings[(req.Page-1)*req.PageSize : req.Page*req.PageSize]
 	}
 	return
 }
