1eb1669e2e050916e6d882484a0cfdfb64ff91dc
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-10-20 21:57:42 +0800

    工具描述细节7

--- uniauth-gf/internal/service/mcp/tools/get_bill_record.go
+++ uniauth-gf/internal/service/mcp/tools/get_bill_record.go
@@ -13,7 +13,7 @@ import (
 
 func RegisterGetBillRecord(s *server.MCPServer) error {
 	tool := mcp.NewTool("get_bill_record",
-		mcp.WithDescription("根据条件查询账单记录。"),
+		mcp.WithDescription("根据条件查询账单记录。有两个类型的账单，需要指定 type：<br>1. type = qp，返回每个配额池名下特定upns相关的账单；<br>2. type = upn，返回每个upn名下这些特定qps相关的账单。<br>数组传空时，则忽略对应的限制。"),
 		mcp.WithInputSchema[v1.GetBillRecordReq](),
 		mcp.WithOutputSchema[v1.GetBillRecordRes](),
 	)
