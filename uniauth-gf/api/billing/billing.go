0bc5692ec02800faeecc3c31d8599846fb3066b9
Yaobo <3060170894@qq.com>
2025-10-21 17:38:59 +0800

    神奇问题修复

--- uniauth-gf/api/billing/billing.go
+++ uniauth-gf/api/billing/billing.go
@@ -17,6 +17,13 @@ type IBillingV1 interface {
 	CheckBalance(ctx context.Context, req *v1.CheckBalanceReq) (res *v1.CheckBalanceRes, err error)
 	CheckTokensUsage(ctx context.Context, req *v1.CheckTokensUsageReq) (res *v1.CheckTokensUsageRes, err error)
 	GetBillingOptions(ctx context.Context, req *v1.GetBillingOptionsReq) (res *v1.GetBillingOptionsRes, err error)
+	GetTodayTotalConsumption(ctx context.Context, req *v1.GetTodayTotalConsumptionReq) (res *v1.GetTodayTotalConsumptionRes, err error)
+	GetProductUsageChart(ctx context.Context, req *v1.GetProductUsageChartReq) (res *v1.GetProductUsageChartRes, err error)
+	GetProductConsumption(ctx context.Context, req *v1.GetProductConsumptionReq) (res *v1.GetProductConsumptionRes, err error)
+	GetActiveUsersNum(ctx context.Context, req *v1.GetActiveUsersNumReq) (res *v1.GetActiveUsersNumRes, err error)
+	GetAllActiveUsers(ctx context.Context, req *v1.GetAllActiveUsersReq) (res *v1.GetAllActiveUsersRes, err error)
+	GetAllServiceName(ctx context.Context, req *v1.GetAllServiceNameReq) (res *v1.GetAllServiceNameRes, err error)
+	GetActiveUserDetail(ctx context.Context, req *v1.GetActiveUserDetailReq) (res *v1.GetActiveUserDetailRes, err error)
 	NDaysProductUsageChart(ctx context.Context, req *v1.NDaysProductUsageChartReq) (res *v1.NDaysProductUsageChartRes, err error)
 	NDaysProductUsageGroup(ctx context.Context, req *v1.NDaysProductUsageGroupReq) (res *v1.NDaysProductUsageGroupRes, err error)
 }
---
+++ uniauth-gf/internal/controller/billing/billing_v1_get_all_service_name.go
@@ -0,0 +1,32 @@
+package billing
+
+import (
+	"context"
+
+	v1 "uniauth-gf/api/billing/v1"
+	"uniauth-gf/internal/dao"
+
+	"github.com/gogf/gf/v2/errors/gerror"
+)
+
+func (c *ControllerV1) GetAllServiceName(ctx context.Context, req *v1.GetAllServiceNameReq) (res *v1.GetAllServiceNameRes, err error) {
+	res = &v1.GetAllServiceNameRes{}
+
+	// 查询所有不重复的服务名称
+	serviceNames, err := dao.BillingCostRecords.Ctx(ctx).
+		Fields("DISTINCT svc").
+		Where("svc IS NOT NULL AND svc != ''").
+		Array()
+	if err != nil {
+		return nil, gerror.Wrap(err, "查询服务名称失败")
+	}
+
+	// 转换为字符串数组
+	res.ServiceName = make([]string, 0, len(serviceNames))
+	for _, name := range serviceNames {
+		if str := name.String(); str != "" {
+			res.ServiceName = append(res.ServiceName, str)
+		}
+	}
+	return res, nil
+}
