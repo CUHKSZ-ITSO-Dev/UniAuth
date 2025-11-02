f4bcc57cf4cf97a1c700b704c6eae436389cdc51
MoyeChen <124240679+MoWeiCH@users.noreply.github.com>
2025-11-02 16:41:43 +0800

    移除不必要的索引创建

--- uniauth-gf/manifest/deploy/migrations/config_internationalization.sql
+++ uniauth-gf/manifest/deploy/migrations/config_internationalization.sql
@@ -11,8 +11,6 @@ CREATE TABLE config_internationalization (
 
 CREATE INDEX idx_config_internationalization_app_id ON config_internationalization (app_id);
 
-CREATE INDEX idx_config_internationalization_key_app ON config_internationalization (key, app_id);
-
 CREATE INDEX idx_config_internationalization_zh_cn ON config_internationalization (zh_cn);
 
 CREATE INDEX idx_config_internationalization_en_us ON config_internationalization (en_us);
